a9a330e62910d48ab95e2ff8c0467fc4b719a98d
Adubbz <a.dubbz@hotmail.com>
2013-07-25 18:53:34 +1000

    Added compatibility with Better Than Horses

--- minecraft_server/net/minecraft/src/biomesoplenty/BiomesOPlenty.java
+++ minecraft_server/net/minecraft/src/biomesoplenty/BiomesOPlenty.java
@@ -3,6 +3,7 @@ package net.minecraft.src.biomesoplenty;
 import net.minecraft.src.FCAddOn;
 import net.minecraft.src.FCAddOnHandler;
 import net.minecraft.src.biomesoplenty.configuration.BOPBiomes;
+import net.minecraft.src.biomesoplenty.integration.BetterThanHorsesIntegration;
 
 public class BiomesOPlenty extends FCAddOn
 {
@@ -18,13 +19,14 @@ public class BiomesOPlenty extends FCAddOn
     public void PreInitialize() 
     {
     	BOPBiomes.init();
+    	BetterThanHorsesIntegration.init();
     }
 	
 	@Override
 	public void Initialize() 
 	{
-		FCAddOnHandler.LogMessage("Biomes O Plenty Version " + bopVersionString + " Initializing...");
-        FCAddOnHandler.LogMessage("Biomes O Plenty Complete.");
+		FCAddOnHandler.LogMessage("[BiomesOPlenty] Biomes O Plenty Version " + bopVersionString + " Initializing...");
+        FCAddOnHandler.LogMessage("[BiomesOPlenty] Biomes O Plenty Initialization Complete.");
 	}
 	
 	@Override
