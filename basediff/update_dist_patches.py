315390ac70703a5538c9df6f7e26571b595f5704
Adubbz <a.dubbz@hotmail.com>
2013-07-27 10:21:16 +1000

    Added script for creating binary patches

--- basediff/update_dist_patches.py
+++ basediff/update_dist_patches.py
@@ -4,7 +4,9 @@ import fnmatch
 import shlex
 import difflib
 import time
+import shutil
 from optparse import OptionParser
+from subprocess import call
 
 def cmdsplit(args):
     if os.sep == '\\':
@@ -28,49 +30,43 @@ def cleanDirs(path):
         
 def main():
     print("Creating patches")
-    
-    parser = OptionParser()
-    parser.add_option('-m', '--mcp-dir', action='store', dest='mcp_dir', help='Path to MCP', default=None)
-    options, _ = parser.parse_args()
-    
     bop_dir = os.path.dirname(os.path.abspath(__file__))
     
-    mcp = os.path.join(bop_dir, 'mcp')
-    if not options.mcp_dir is None:
-        mcp = os.path.abspath(options.mcp_dir)
-    elif os.path.isfile(os.path.join('..', 'runtime', 'commands.py')):
-        mcp = os.path.abspath('..')
-    
     patchd = os.path.normpath(os.path.join(bop_dir, 'patches'))
-    base = os.path.normpath(os.path.join(mcp, 'src_base'))
-    work = os.path.normpath(os.path.join(mcp, 'src'))
+    base = os.path.normpath(os.path.join(bop_dir, 'base'))
+    work = os.path.normpath(os.path.join(bop_dir, 'work'))
     
     for path, _, filelist in os.walk(work, followlinks=True):
-        for cur_file in fnmatch.filter(filelist, '*.java'):
+        for cur_file in fnmatch.filter(filelist, '*.class'):
             file_base = os.path.normpath(os.path.join(base, path[len(work)+1:], cur_file)).replace(os.path.sep, '/')
             file_work = os.path.normpath(os.path.join(work, path[len(work)+1:], cur_file)).replace(os.path.sep, '/')
-            if not os.path.isfile(file_base):
-            	print("Missing base file %s"%(file_base))
-            	continue
-            fromlines = open(file_base, 'U').readlines()
-            tolines = open(file_work, 'U').readlines()
             
-            patch = ''.join(difflib.unified_diff(fromlines, tolines, '../' + file_base[len(mcp)+1:], '../' + file_work[len(mcp)+1:], '', '', n=3))
+            if not os.path.isfile(file_base):
+            	print("Missing base file %s using work file..."%(file_base))
+            	non_btw_edits = os.path.normpath(os.path.join(bop_dir, 'nonbtwedits'))
+            	non_btw_edits_copy = os.path.normpath(os.path.join(non_btw_edits, path[len(work)+1:])).replace(os.path.sep, '/')
+            	if not os.path.exists(non_btw_edits_copy):
+                    os.makedirs(non_btw_edits_copy)
+            	shutil.copyfile(file_work, os.path.normpath(os.path.join(non_btw_edits_copy, cur_file)).replace(os.path.sep, '/'))
+            	continue
+            
+            #patch = ''.join(difflib.unified_diff(fromlines, tolines, '../' + file_base[len(bop_dir)+1:], '../' + file_work[len(bop_dir)+1:], '', '', n=3))
             patch_dir = os.path.join(patchd, path[len(work)+1:])
             patch_file = os.path.join(patch_dir, cur_file + '.patch')
             
-            if len(patch) > 0:
-                print patch_file[len(patchd)+1:]
-                patch = patch.replace('\r\n', '\n')
+            print patch_file[len(patchd)+1:]
                 
-                if not os.path.exists(patch_dir):
-                    os.makedirs(patch_dir)
-                with open(patch_file, 'wb') as fh:
-                    fh.write(patch)
-            else:
-                if os.path.isfile(patch_file):
-                    print("Deleting empty patch: %s"%(patch_file))
-                    os.remove(patch_file)
+            if not os.path.exists(patch_dir):
+                os.makedirs(patch_dir)
+            
+            call(["bsdiff", file_base[len(bop_dir)+1:], file_work[len(bop_dir)+1:], patch_file])
+            
+            #if len(patch) > 0:
+
+            #else:
+                #if os.path.isfile(patch_file):
+                    #print("Deleting empty patch: %s"%(patch_file))
+                    #os.remove(patch_file)
                     
 
     cleanDirs(patchd)
